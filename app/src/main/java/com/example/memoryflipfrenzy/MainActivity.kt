f8c58d02c95421e292d4563eaf0095e75bedb7e8
Tanuj Khanna <tanujkhanna72@gmail.com>
2023-07-27 01:15:58 +0530

    Creating the basic layout

--- app/src/main/java/com/example/memoryflipfrenzy/MainActivity.kt
+++ app/src/main/java/com/example/memoryflipfrenzy/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.memoryflipfrenzy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var rvBoard : RecyclerView
+    lateinit var tvNumMoves : TextView
+    lateinit var tvNumPairs : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvBoard = findViewById(R.id.rvBoard)
+        tvNumMoves = findViewById(R.id.tvNumMoves)
+        tvNumPairs = findViewById(R.id.tvNumPairs)
     }
 }
