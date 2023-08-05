4d8f8d6320f6dc03ea0678b6e1cc906bfe4afdd8
Tanuj Khanna <tanujkhanna72@gmail.com>
2023-08-05 19:15:48 +0530

    Creating image intent and requesting permission

--- app/src/main/java/com/example/memoryflipfrenzy/CreateActivity.kt
+++ app/src/main/java/com/example/memoryflipfrenzy/CreateActivity.kt
@@ -1,17 +1,28 @@
 package com.example.memoryflipfrenzy
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.memoryflipfrenzy.models.BoardSize
 import com.example.memoryflipfrenzy.utils.EXTRA_BOARD_SIZE
+import com.example.memoryflipfrenzy.utils.isPermissionGranted
+import com.example.memoryflipfrenzy.utils.requestPermission
 
 class CreateActivity : AppCompatActivity() {
+
+    companion object {
+        const val PICK_PHOTO_CODE = 128
+        const val READ_EXTERNAL_PHOTOS_CODE = 891
+        const val READ_PHOTOS_PERMISSION = android.Manifest.permission.READ_EXTERNAL_STORAGE
+    }
 
     private lateinit var rvImagePicker : RecyclerView
     private lateinit var etGameName : EditText
@@ -34,9 +45,31 @@ class CreateActivity : AppCompatActivity() {
         numImagesRequired = boardSize.getNumPairs()
         supportActionBar?.title = "Choose Pics (0 / $numImagesRequired)"
 
-        rvImagePicker.adapter = ImagePickerAdapter(this, chosenImageUris, boardSize)
+        rvImagePicker.adapter = ImagePickerAdapter(this, chosenImageUris, boardSize, object: ImagePickerAdapter.ImageClickListener{
+            override fun onPlaceHolderClicked() {
+                if(isPermissionGranted(this@CreateActivity, READ_PHOTOS_PERMISSION))
+                    launchIntentForPhotos()
+                else
+                    requestPermission(this@CreateActivity, READ_PHOTOS_PERMISSION, READ_EXTERNAL_PHOTOS_CODE)
+            }
+
+        })
         rvImagePicker.setHasFixedSize(true)
         rvImagePicker.layoutManager = GridLayoutManager(this, boardSize.getWidth())
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode == READ_EXTERNAL_PHOTOS_CODE) {
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                launchIntentForPhotos()
+            } else {
+                Toast.makeText(this, "In order to create a custom game, you need to provide access to your photos", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -45,5 +78,12 @@ class CreateActivity : AppCompatActivity() {
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun launchIntentForPhotos() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+        startActivityForResult(Intent.createChooser(intent, "Choose pics"), PICK_PHOTO_CODE)
     }
 }
